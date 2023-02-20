5689495bdfb78fc5c2881ec7cb5ca0d687ff8199
Justin <justin.r.pickering@gmail.com>
2023-02-19 19:06:24 -0500

    set up initial play game screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css"
 
 export default function App() {
+
+  const [inGame, setInGame] = React.useState(false)
+
+  const [triviaQuestions, setTriviaQuestions] = React.useState([])
+
+  function startGame() {
+    setInGame(true)
+  }
+
   return (
-    <div className="App">
+    <div className="app">
+      {inGame ? 
+      
+      
+      null 
+      : 
+      <div className="start-quiz">
+        <h1>Quizzical</h1>
+        <button onClick={startGame}>Start Quiz</button>
+      </div>
+      }
+
+
 
     </div>
   );
